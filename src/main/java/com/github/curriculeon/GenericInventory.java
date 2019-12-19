ea3bc587a64303dacdc9e239aa007956186a1473
unknown <tierney_mellen@yahoo.com>
2019-12-19 10:27:03 -0500

    first commit, I think I'm done

--- src/main/java/com/github/curriculeon/GenericInventory.java
+++ src/main/java/com/github/curriculeon/GenericInventory.java
@@ -1,31 +1,53 @@
 package com.github.curriculeon;
 
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by leon on 12/16/2019.
  */
-public class GenericInventory<SomeType> implements InventoryInterface<SomeType> {
+public class GenericInventory<SomeType> implements InventoryInterface<SomeType>, Iterable<SomeType> {
+    private List<SomeType> inventory = new ArrayList<>();
+
+    public GenericInventory() { }
+
+    public GenericInventory(List<SomeType> inventory) {
+        this.inventory = inventory;
+    }
+
     @Override
     public void add(SomeType someObject) {
 
+        inventory.add(someObject);
     }
 
     @Override
     public Boolean contains(SomeType someObject) {
-        return null;
+
+        return inventory.contains(someObject);
     }
 
     @Override
     public SomeType get(int indexOfElement) {
-        return null;
+
+        return inventory.get(indexOfElement);
     }
 
     @Override
     public SomeType remove(int indexOfElement) {
-        return null;
+
+        return inventory.remove(indexOfElement);
     }
 
     @Override
-    public SomeType[] toArray(SomeType[] objectsToBeAdded) {
-        return null;
+    public SomeType[] toArray(SomeType[] arrayToBePopulated) {
+        return inventory.toArray(arrayToBePopulated);
+    }
+
+    @Override
+    public Iterator<SomeType> iterator() {
+        return inventory.iterator();
     }
 }
